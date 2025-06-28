ebca7c1383eef742d08b8bc311f945013c18cb85
deep04102004 <dkb6865@gmail.com>
2025-06-28 23:24:28 +0530

    237. Delete Node in a Linked List

---
+++ Linked List/Delete_node.cpp
@@ -0,0 +1,50 @@
+//237. Delete Node in a Linked List
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
+// Helper function to print the list
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Creating the linked list: 4 -> 5 -> 1 -> 9
+    ListNode* head = new ListNode(4);
+    head->next = new ListNode(5);
+    head->next->next = new ListNode(1);
+    head->next->next->next = new ListNode(9);
+
+    cout << "Original List: ";
+    printList(head);
+
+    // Deleting node with value 5 (by passing pointer to node with value 5)
+    Solution sol;
+    sol.deleteNode(head->next); // deletes node with value 5
+
+    cout << "After Deletion: ";
+    printList(head);
+
+    return 0;
+}
