16841660b9ff223cfb34ae70a93e7a5ec1008c5b
deep04102004 <dkb6865@gmail.com>
2025-09-05 17:55:13 +0530

    creation of binary tree

---
+++ Tree/Creation_of_binary_tree.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class node{
+  public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+       this->data = d;
+       this->left = NULL;
+       this->right = NULL;
+    }
+
+    
+};
+
+node* buildtree(node* root){
+    cout<<"Enter the data: "<<endl;
+    int data;
+    cin>>data;
+
+    if(data == -1){
+        return NULL;
+    }
+    root = new node(data);
+    
+    
+    cout<<"Enter data for Left Node "<<data<<endl;
+    root->left = buildtree(root->left);
+    cout<<"Enter data for right node "<<data<<endl;
+    root->right = buildtree(root->right);
+
+    return root;
+}
+
+int main(){
+    node* root = NULL;
+    root = buildtree(root);
+
+    return 0;
+}
